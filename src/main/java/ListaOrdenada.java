344e210b7f4fbd8486729027c7ece728b418d962
RaduRaresGeorge <rradu@myuax.com>
2024-11-26 19:00:32 +0100

    Clase ListaOrdenda

--- src/main/java/ListaOrdenada.java
+++ src/main/java/ListaOrdenada.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class ListaOrdenada {
+import elementos.*;
+
+ /*
+  * Clase Genérica que acepta únicamente objetos que implementen la interfaz Comparable
+  */
+
+public class ListaOrdenada<T extends Comparable<T>> {
+    private List<T> elementos;
+
+    public ListaOrdenada() {
+        this.elementos = new ArrayList<>();
+    }
+
+    public void agregar(T elemento) {
+        elementos.add(elemento);
+        Collections.sort(elementos);
+    }
+
+    public List<T> getElementos() {
+        return elementos;
+    }
 }
