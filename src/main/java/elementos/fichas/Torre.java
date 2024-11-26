e65a7de887e2800dc0b6ee7025b9bfbb5301b103
RaduRaresGeorge <rradu@myuax.com>
2024-11-26 20:04:09 +0100

    Métodos Clase Torre

--- src/main/java/elementos/fichas/Torre.java
+++ src/main/java/elementos/fichas/Torre.java
@@ -1,10 +1,27 @@
 package elementos.fichas;
 
 import elementos.Ficha;
+import elementos.interfaz.TiposColor;
 
 public class Torre extends Ficha {
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+
+    public Torre(TiposColor color){
+        super(color);
     }
+
+    @Override
+    protected int getRank() {
+        return 5;
+    }
+
+    @Override
+    public boolean validarMovimiento(int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+        return filaOrigen == filaDestino ^ columnaOrigen == columnaDestino;
+    }
+
+    @Override
+    public String obtenerRepresentacion() {
+        return "T";
+    }
+
 }
