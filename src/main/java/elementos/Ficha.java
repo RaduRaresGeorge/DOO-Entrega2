1c8b9792ad688d4a3bc9ccee1036356190291773
RaduRaresGeorge <rradu@myuax.com>
2024-11-26 19:07:50 +0100

    Clase Ficha

--- src/main/java/elementos/Ficha.java
+++ src/main/java/elementos/Ficha.java
@@ -4,9 +4,17 @@ package elementos;
 *  Implementar CompareTo con la siguiente jerarquia:
 *   Blancas > Negras
 *   Rey > Reina > Alfil > Caballo > Torre > Peon
+*
+*   Métodos:
+*       - compareTo
+*       - (abstract) boolean validarMovimiento(filaOrigen, columnaOrigen, filaDestino, columnaDestino)
+*       - (abstract) String obtenerRepresentacion()
+*       - toString (devuelve el color y la representación)
 * */
 
-public abstract class Ficha implements Comparable {
+import elementos.interfaz.Color;
+
+public abstract class Ficha implements Comparable, Color {
 
     @Override
     public int compareTo(Object o) {
