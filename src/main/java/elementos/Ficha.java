d81f821f41dc158600f1ab470132d77256c345ae
RaduRaresGeorge <rradu@myuax.com>
2024-11-26 19:03:19 +0100

    Clase Ficha

--- src/main/java/elementos/Ficha.java
+++ src/main/java/elementos/Ficha.java
@@ -1,4 +1,15 @@
 package elementos;
 
-public class Ficha {
+/*
+*  Implementar CompareTo con la siguiente jerarquia:
+*   Blancas > Negras
+*   Rey > Reina > Alfil > Caballo > Torre > Peon
+* */
+
+public abstract class Ficha implements Comparable {
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
