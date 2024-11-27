1dafa972fbc6c43239501f1445e5dee3a1498469
RaduRaresGeorge <rradu@myuax.com>
2024-11-27 11:44:42 +0100

    Métodos Clase Alfil

--- src/main/java/elementos/fichas/Alfil.java
+++ src/main/java/elementos/fichas/Alfil.java
@@ -1,10 +1,27 @@
 package elementos.fichas;
 
 import elementos.Ficha;
+import elementos.interfaz.TiposColor;
 
 public class Alfil extends Ficha {
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+
+    public Alfil(TiposColor color){
+        super(color);
     }
+
+    @Override
+    protected int getRank() {
+        return 3;
+    }
+
+    @Override
+    public boolean validarMovimiento(int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+        return Math.abs(filaOrigen - filaDestino) == Math.abs(columnaOrigen - columnaDestino);
+    }
+
+    @Override
+    public String obtenerRepresentacion() {
+        return "A";
+    }
+
 }
