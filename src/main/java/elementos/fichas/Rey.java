5d679859c16668566695b35ba19ab8f31679dc54
RaduRaresGeorge <rradu@myuax.com>
2024-11-27 11:52:03 +0100

    Métodos Clase Rey

--- src/main/java/elementos/fichas/Rey.java
+++ src/main/java/elementos/fichas/Rey.java
@@ -1,10 +1,27 @@
 package elementos.fichas;
 
 import elementos.Ficha;
+import elementos.interfaz.TiposColor;
 
 public class Rey extends Ficha {
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+
+    public Rey(TiposColor color){
+        super(color);
     }
+
+    @Override
+    protected int getRank() {
+        return 1;
+    }
+
+    @Override
+    public boolean validarMovimiento(int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+        return Math.abs(filaOrigen - filaDestino) <= 1 && Math.abs(columnaOrigen - columnaDestino) <= 1;
+    }
+
+    @Override
+    public String obtenerRepresentacion() {
+        return "K";
+    }
+
 }
