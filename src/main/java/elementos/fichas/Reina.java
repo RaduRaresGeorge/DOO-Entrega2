6d098b22b713669a911bdf2d284e8996616307da
RaduRaresGeorge <rradu@myuax.com>
2024-11-27 11:53:04 +0100

    Métodos Clase Reina

--- src/main/java/elementos/fichas/Reina.java
+++ src/main/java/elementos/fichas/Reina.java
@@ -1,10 +1,27 @@
 package elementos.fichas;
 
 import elementos.Ficha;
+import elementos.interfaz.TiposColor;
 
 public class Reina extends Ficha {
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+
+    public Reina(TiposColor color){
+        super(color);
     }
+
+    @Override
+    protected int getRank() {
+        return 9;
+    }
+
+    @Override
+    public boolean validarMovimiento(int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+        return filaOrigen == filaDestino ^ columnaOrigen == columnaDestino || Math.abs(filaOrigen - filaDestino) == Math.abs(columnaOrigen - columnaDestino);
+    }
+
+    @Override
+    public String obtenerRepresentacion() {
+        return "Q";
+    }
+
 }
