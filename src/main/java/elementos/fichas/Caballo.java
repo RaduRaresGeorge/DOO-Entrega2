603f47b4104d6a7ad4dee6e2d13eb17d68e6b9ab
RaduRaresGeorge <rradu@myuax.com>
2024-11-27 11:46:40 +0100

    Métodos Clase Caballo

--- src/main/java/elementos/fichas/Caballo.java
+++ src/main/java/elementos/fichas/Caballo.java
@@ -1,10 +1,28 @@
 package elementos.fichas;
 
 import elementos.Ficha;
+import elementos.interfaz.TiposColor;
 
 public class Caballo extends Ficha {
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+
+    public Caballo(TiposColor color){
+        super(color);
     }
+
+    @Override
+    protected int getRank() {
+        return 4;
+    }
+
+    @Override
+    public boolean validarMovimiento(int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+        return (Math.abs(filaOrigen - filaDestino) == 2 && Math.abs(columnaOrigen - columnaDestino) == 1) ||
+                (Math.abs(filaOrigen - filaDestino) == 1 && Math.abs(columnaOrigen - columnaDestino) == 2);
+    }
+
+    @Override
+    public String obtenerRepresentacion() {
+        return "C";
+    }
+
 }
