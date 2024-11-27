4d48bb677903bb72609b8ebdd83da16c588259c0
RaduRaresGeorge <rradu@myuax.com>
2024-11-27 11:50:30 +0100

    Métodos Clase Peón

--- src/main/java/elementos/fichas/Peon.java
+++ src/main/java/elementos/fichas/Peon.java
@@ -1,10 +1,33 @@
 package elementos.fichas;
 
 import elementos.Ficha;
+import elementos.interfaz.TiposColor;
 
 public class Peon extends Ficha {
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+
+    public Peon(TiposColor color){
+        super(color);
     }
+
+    @Override
+    protected int getRank() {
+        return 6;
+    }
+
+    @Override
+    public boolean validarMovimiento(int filaOrigen, int columnaOrigen, int filaDestino, int columnaDestino) {
+        if (this.getColor() == TiposColor.BLANCO) {
+            return (filaOrigen - filaDestino == 1 && columnaOrigen == columnaDestino) ||
+                   (filaOrigen - filaDestino == 1 && Math.abs(columnaOrigen - columnaDestino) == 1);
+        } else {
+            return (filaOrigen - filaDestino == -1 && columnaOrigen == columnaDestino) ||
+                   (filaOrigen - filaDestino == -1 && Math.abs(columnaOrigen - columnaDestino) == 1);
+        }
+    }
+
+    @Override
+    public String obtenerRepresentacion() {
+        return "P";
+    }
+
 }
