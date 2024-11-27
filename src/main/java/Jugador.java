1a9e677a0015ffd03cc7c19a4048626d685a5bdc
RaduRaresGeorge <rradu@myuax.com>
2024-11-27 12:01:22 +0100

    Métodos Clase Jugador

--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -1,16 +1,66 @@
+import elementos.Ficha;
+import elementos.fichas.*;
 import elementos.interfaz.*;
 
 public class Jugador implements Color {
 
-    /*
-    Atributos
-        Lista ordenada que solo pueda implementar fichas
-        Comidas y en posesion
-    Constructor
-        Crear 8 peones, 2 torres, 2 caballos, 2 alfiles, 1 reina y 1 rey
-    Metodos
-        Indicar que una ficha ha sido comida y cambiarla de array
-        toString que muestre las fichas comidas de cada jugador de manera ordenada
-    * */
+    private Ficha[] fichasEnPosesion;
+    private Ficha[] fichasComidas;
+    private TiposColor color;
 
+    public Jugador(TiposColor color){
+        fichasEnPosesion = new Ficha[16];
+        fichasComidas = new Ficha[16];
+        this.color = color;
+        int i = 0;
+        for (int j = 0; j < 8; j++) {
+            fichasEnPosesion[i++] = new Peon(this.color);
+        }
+        for (int j = 0; j < 2; j++) {
+            fichasEnPosesion[i++] = new Torre(this.color);
+        }
+        for (int j = 0; j < 2; j++) {
+            fichasEnPosesion[i++] = new Caballo(this.color);
+        }
+        for (int j = 0; j < 2; j++) {
+            fichasEnPosesion[i++] = new Alfil(this.color);
+        }
+        fichasEnPosesion[i++] = new Reina(this.color);
+        fichasEnPosesion[i] = new Rey(this.color);
+    }
+
+    public TiposColor getColor(){
+        return this.color;
+    }
+
+    public void setColor(TiposColor color){
+        this.color = color;
+    }
+
+    public void comerFicha(Ficha ficha){
+        for (int i = 0; i < fichasEnPosesion.length; i++) {
+            if (fichasEnPosesion[i] == ficha){
+                fichasEnPosesion[i] = null;
+                break;
+            }
+        }
+        for (int i = 0; i < fichasComidas.length; i++) {
+            if (fichasComidas[i] == null){
+                fichasComidas[i] = ficha;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Fichas comidas por el jugador ").append(this.color).append(": ");
+        for (Ficha ficha : fichasComidas) {
+            if (ficha != null){
+                sb.append(ficha).append(", ");
+            }
+        }
+        return sb.toString();
+    }
 }
